d3aa6a5b14aa50b3079eb76877f149ec51e1c43d
anjali-8001 <93631333+anjali-8001@users.noreply.github.com>
2023-09-11 20:33:44 +0530

    Anjali TrackCase

---
+++ client/src/Components/TrackCase.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "../CSS/TrackCaseStyle.css";
+import { RiSearchLine } from "react-icons/ri";
+
+function TrackCase() {
+  const [caseData,setCaseData] = useState({});
+  const obj = {
+    name: "Arushi",
+    age: "34",
+    gender: "Female",
+    address: "street no. 42",
+    lawyerName: "Kapil",
+    caseName: "xyz",
+    mobileNumber: "9234667007",
+  };
+
+  const handleClick = (event) =>{
+    setCaseData(obj);
+    event.preventDefault();
+  }
+
+  return (
+    <div className="mainBox">
+      <div className="trackCase">
+        <h1 className="heading">Track a Case</h1>
+        <div className="searchBar">
+          <RiSearchLine className="searchIcon" />
+          <form action="/track">
+            <input
+              className="trackCaseInput"
+              type="text"
+              name="trackingId"
+              placeholder="Enter Case ID to search"
+            />
+            <button className="trackButton" type="submit" onClick={handleClick}>
+              Track
+            </button>
+          </form>
+        </div>
+        <div className="caseDetails">
+          <h2>Case Details</h2>
+          <div className="caseData">
+            {Object.entries(caseData).map((key) => {
+              return (
+                <div className="caseDataElement">
+                  <span className="caseDataHeading">{key[0]} : </span>
+                  <span className="caseDataValue">{key[1]}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TrackCase;
