5ee7e03d8c8d0a4f7ca20691ef5eec614786958c
Devansh <devanshsahni@gmail.com>
2023-09-12 10:08:56 +0530

    backend TrackCase

--- client/src/TrackCase.js
+++ client/src/TrackCase.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/TrackCaseStyle.css";
 import { RiSearchLine } from "react-icons/ri";
 
 function TrackCase() {
+  const[caseID, setCaseId]=useState();
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+
+    const response = await fetch(`${process.env.REACT_APP_BASE_URL}/search`,{
+      method:"POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials:"include",
+      body: JSON.stringify({
+        caseID,
+      }),
+    })
+    console.log(response);
+  }
   return (
     <div className="mainBox">
       <div className="trackCase">
         <h1 className="heading">Track a Case</h1>
         <div className="searchBar">
           <RiSearchLine className="searchIcon" />
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               name="trackingId"
               placeholder="Enter Case ID to search"
+              value={caseID}
+              onChange={(e)=>{setCaseId(e.target.value)}}
             />
             <button className="trackButton" type="submit">
               Track
